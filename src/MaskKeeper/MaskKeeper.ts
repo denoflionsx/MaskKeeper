fb40aaa9ab0eec93750b8ebff953baf580a005aa
Denoflions <denvstheworld@gmail.com>
2021-10-11 09:03:28 -0400

    Update for Z64O

--- src/MaskKeeper/MaskKeeper.ts
+++ src/MaskKeeper/MaskKeeper.ts
@@ -1,22 +1,11 @@
-import { IPlugin, IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
+import { IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
 import { MaskKeeperClient } from './MaskKeeperClient';
 import { SidedProxy, ProxySide } from 'modloader64_api/SidedProxy/SidedProxy';
 
-class MaskKeeper implements IPlugin {
+export default class MaskKeeper {
 
     ModLoader!: IModLoaderAPI;
     @SidedProxy(ProxySide.CLIENT, MaskKeeperClient)
     clientSide!: MaskKeeperClient;
 
-    preinit(): void {
-    }
-    init(): void {
-    }
-    postinit(): void {
-    }
-    onTick(frame?: number | undefined): void {
-    }
-
 }
-
-module.exports = MaskKeeper;
--- src/MaskKeeper/MaskKeeperClient.ts
+++ src/MaskKeeper/MaskKeeperClient.ts
@@ -1,20 +1,22 @@
 
 import { InjectCore } from 'modloader64_api/CoreInjection';
-import { IOOTCore, Age, OotEvents } from 'modloader64_api/OOT/OOTAPI';
 import { IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
 import { ModLoaderAPIInject } from 'modloader64_api/ModLoaderAPIInjector';
 import { EventHandler } from 'modloader64_api/EventHandler';
+import { IZ64Main } from 'Z64Lib/API/Common/IZ64Main';
+import { Z64 } from 'Z64Lib/API/imports';
+import { AgeOrForm } from 'Z64Lib/API/Common/Z64API';
 
 export class MaskKeeperClient {
 
     @ModLoaderAPIInject()
     ModLoader!: IModLoaderAPI;
     @InjectCore()
-    core!: IOOTCore;
+    core!: IZ64Main;
 
-    @EventHandler(OotEvents.ON_SCENE_CHANGE)
+    @EventHandler(Z64.OotEvents.ON_SCENE_CHANGE)
     onSceneChange(scene: number) {
-        if (this.core.save.age !== Age.CHILD && !this.ModLoader.isModLoaded("ChildGear")) {
+        if (this.core.OOT!.save.age !== AgeOrForm.CHILD && !this.ModLoader.isModLoaded("ChildGear")) {
             return;
         }
         let c_buttons: Buffer = this.ModLoader.emulator.rdramReadBuffer(global.ModLoader.save_context + 0x0068, 0x4);
@@ -25,7 +27,7 @@ export class MaskKeeperClient {
                 mask = c_buttons[i] - 0x23;
             }
         }
-        this.core.link.mask = mask;
+        this.core.OOT!.link.mask = mask;
     }
 
 }
