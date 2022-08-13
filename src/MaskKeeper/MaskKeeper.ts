ce674cc582b2c7d8758bc17377e4f0f34a1d0d12
den <denvstheworld@gmail.com>
2022-08-13 12:51:36 -0400

    Make sure mod only runs in Oot

--- src/MaskKeeper/MaskKeeper.ts
+++ src/MaskKeeper/MaskKeeper.ts
@@ -1,11 +1,13 @@
 import { IModLoaderAPI } from 'modloader64_api/IModLoaderAPI';
 import { MaskKeeperClient } from './MaskKeeperClient';
 import { SidedProxy, ProxySide } from 'modloader64_api/SidedProxy/SidedProxy';
+import { Z64_GAME } from "Z64Lib/src/Common/types/GameAliases";
+import { Z64LibSupportedGames } from "Z64Lib/API/Utilities/Z64LibSupportedGames";
 
 export default class MaskKeeper {
 
     ModLoader!: IModLoaderAPI;
-    @SidedProxy(ProxySide.CLIENT, MaskKeeperClient)
+    @SidedProxy(ProxySide.CLIENT, MaskKeeperClient, "Z64Lib", () => { return Z64_GAME === Z64LibSupportedGames.OCARINA_OF_TIME })
     clientSide!: MaskKeeperClient;
 
 }
